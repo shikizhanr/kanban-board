9ab80f94b8cdeb220221d7216853e91b343bdf53
Higg89 <Higg89SN@gmail.com>
2025-04-01 15:46:44 +0500

    v0.7
    Продолжаю подготовку, исправил уязвимость

--- migrations/env.py
+++ migrations/env.py
@@ -7,6 +7,10 @@ from alembic import context
 import os
 import sys
 
+
+from dotenv import load_dotenv
+load_dotenv()
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from backend.app.database import Base
@@ -64,12 +68,25 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+
+    # Получение URL из переменных окружения
+    database_url = os.getenv("DATABASE_URL")
+
+    config.set_main_option("sqlalchemy.url", database_url)
+
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
-
+"""
     with connectable.connect() as connection:
         context.configure(
             connection=connection, target_metadata=target_metadata
